51e00b4803e0e6963077802950253a8294407d65
ernesto <274116253+ernestoc29@users.noreply.github.com>
2026-07-18 16:06:15 -0400

    add library render function

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-const container = document.querySelector(".container");
+const libraryContainer = document.querySelector(".library");
 
-const myLibrary = [];
+const myLibrary = [{title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 295, read: false}, {title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 295, read: true}];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read ?? false;
-
-    this.info = function() {
-        const status = this.read ? "already read" : "not read yet"
-
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`
-    };
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -20,3 +14,29 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
     return newBook;
 }
+
+myLibrary.forEach(book => {
+    const bookDiv = document.createElement("div");
+
+    const titleDisplay = document.createElement("p");
+    const authorDisplay = document.createElement("p");
+    const pagesDisplay = document.createElement("p");
+    const readDisplay = document.createElement("p");
+
+    titleDisplay.textContent = book.title;
+    authorDisplay.textContent = book.author;
+    pagesDisplay.textContent = book.pages;
+
+    if (book.read === false) {
+        readDisplay.textContent = "Not yet read"
+    } else {
+        readDisplay.textContent = "Already read"
+    };
+
+    bookDiv.appendChild(titleDisplay);
+    bookDiv.appendChild(authorDisplay);
+    bookDiv.appendChild(pagesDisplay);
+    bookDiv.appendChild(readDisplay);
+
+    libraryContainer.appendChild(bookDiv);
+})
